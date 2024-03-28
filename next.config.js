70f84c19387f54725a25cfcc3fb4fbfdda3cce3c
Dorian Wilhelm <wilhelm.dorian@pm.me>
2024-03-28 08:54:56 +0100

    - mirror next config of other project

--- next.config.js
+++ next.config.js
@@ -1,6 +1,28 @@
 /** @type {import('next').NextConfig} */
+const webpack = require("webpack");
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+  images: {
+    domains: ["arweave.net"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.arweave.net",
+      },
+    ],
+    unoptimized: true,
+  },
+  webpack: (config, options) => {
+    const plugins = [
+      ...config.plugins,
+      new webpack.NormalModuleReplacementPlugin(/node:/, (resource) => {
+        resource.request = resource.request.replace(/^node:/, "");
+      }),
+    ];
+    return { ...config, plugins };
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
