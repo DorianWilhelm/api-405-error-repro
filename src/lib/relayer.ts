98b1554badbf64564b4db07b4969d732440781cb
Dorian Wilhelm <wilhelm.dorian@pm.me>
2024-03-31 16:42:22 +0200

    test

--- src/lib/relayer.ts
+++ src/lib/relayer.ts
@@ -1,6 +1,9 @@
-import { Relayer } from "@openzeppelin/defender-relay-client";
-
-import { privateKeyToAccount } from "viem/accounts";
+// import { Relayer } from "@openzeppelin/defender-relay-client";
+import {
+  DefenderRelayProvider,
+  DefenderRelaySigner,
+} from "defender-relay-client/lib/ethers";
+import { toBytes } from "viem";
 
 const defenderCredentials = {
   apiKey: process.env.DEFENDER_RELAY_API_KEY!,
@@ -8,18 +11,14 @@ const defenderCredentials = {
 };
 
 export const relayerSign = async (network: number, message: `0x${string}`) => {
-  if (31337 == network) {
-    //that's hardhat's 0th account, so it's safe ;)
-    const _privateKey =
-      "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
-    const account = privateKeyToAccount(_privateKey);
-
-    const signature = await account.signMessage({ message: { raw: message } });
-    return { signature, address: account.address };
-  } else {
-    const signer = new Relayer(defenderCredentials);
-    const { sig } = await signer.sign({ message });
-    const { address } = await signer.getRelayer();
-    return { signature: sig, address };
-  }
+  const signer = new DefenderRelaySigner(
+    defenderCredentials,
+    new DefenderRelayProvider(defenderCredentials),
+    {
+      speed: "fast",
+    }
+  );
+  const signature = await signer.signMessage(toBytes(message));
+  const address = await signer.getAddress();
+  return { signature, address };
 };
